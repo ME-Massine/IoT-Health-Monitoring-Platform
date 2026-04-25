16ff488092734d6c81772949e906429b12b909aa
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2026-04-25 20:57:39 +0100

    fix: complete global exception handling classes

--- backend/src/main/java/com/iothealth/backend/exception/BadRequestException.java
+++ backend/src/main/java/com/iothealth/backend/exception/BadRequestException.java
@@ -1,4 +1,8 @@
 package com.iothealth.backend.exception;
 
-public class BadRequestException {
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
 }
